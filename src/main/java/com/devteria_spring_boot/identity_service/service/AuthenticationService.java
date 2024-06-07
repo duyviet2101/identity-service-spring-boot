bea32e0e7ee2b77da1081b9fb6a81087c558aeda
duyviet2101 <whoisduyviet@gmail.com>
2024-06-07 23:53:25 +0700

    #7. tao, ky JWT

--- src/main/java/com/devteria_spring_boot/identity_service/service/AuthenticationService.java
+++ src/main/java/com/devteria_spring_boot/identity_service/service/AuthenticationService.java
@@ -1,28 +1,80 @@
 package com.devteria_spring_boot.identity_service.service;
 
 import com.devteria_spring_boot.identity_service.dto.request.AuthenticationRequest;
+import com.devteria_spring_boot.identity_service.dto.response.AuthenticationResponse;
 import com.devteria_spring_boot.identity_service.exception.AppException;
 import com.devteria_spring_boot.identity_service.exception.ErrorCode;
 import com.devteria_spring_boot.identity_service.repository.UserRepository;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationService {
+    private static final Logger log = LoggerFactory.getLogger(AuthenticationService.class);
     UserRepository userRepository;
 
-    public boolean authenticate(AuthenticationRequest request) {
+    @NonFinal
+    protected static final String SIGNER_KEY = "O31iXtMnUJgiMvuYFqwHssLqMen295/7Ubuz41EYZS2tuUD5C5Ldmo6Jgxa3UnOM";
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
         var user = userRepository.findByUsername(request.getUsername())
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
 
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
 
-        return passwordEncoder.matches(request.getPassword(), user.getPassword());
+        boolean authenticated = passwordEncoder.matches(request.getPassword(), user.getPassword());
+
+        if (!authenticated) {
+            throw new AppException(ErrorCode.UNAUTHENTICATED);
+        }
+
+        var token = generateToken(request.getUsername());
+
+        return AuthenticationResponse.builder()
+                .token(token)
+                .authenticated(true)
+                .build();
+    }
+
+    private String generateToken(String username) {
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                .subject(username) // user dang nhap
+                .issuer("devteria.com") // xac dinh token duoc issue tu ai?
+                .issueTime(new Date())
+                .expirationTime(new Date(
+                        Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()
+                ))
+                .claim("customClaim", "Custom")
+                .build();
+
+        Payload payload = new Payload(jwtClaimsSet.toJSONObject());
+
+        JWSObject jwsObject = new JWSObject(header, payload);
+
+        try {
+            jwsObject.sign(new MACSigner(SIGNER_KEY.getBytes()));
+            return jwsObject.serialize();
+        } catch (JOSEException e) {
+            log.error("Cannot create token", e);
+            throw new RuntimeException(e);
+        }
     }
 }
